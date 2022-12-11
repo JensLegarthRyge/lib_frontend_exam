1cb3200094f75d7e13b4f782258310fbfb3e2e3e
JensLegarthRyge <jens.legarth.ryge@gmail.com>
2022-12-11 14:03:04 +0100

    Initial commit

--- index.js
+++ index.js
@@ -6,14 +6,8 @@ import {
   renderTemplate,
 
 } from "./utils.js"
-
-import { load } from "./pages/cars/cars.js"
-import { load as loadV2 } from "./pages/cars-bootstrap/cars.js"
-
 window.addEventListener("load", async () => {
   const templateHome = await loadHtml("./pages/home/home.html")
-  const templateCars = await loadHtml("./pages/cars/cars.html")
-  const templateCarsBootstrap = await loadHtml("./pages/cars-bootstrap/cars.html")
 
   const router = new Navigo("/", { hash: true });
   window.router = router
@@ -28,13 +22,7 @@ window.addEventListener("load", async () => {
     })
     .on({
       "/": () => renderTemplate(templateHome, "content"),
-      "/cars": (match) => {
-        renderTemplate(templateCars, "content")
-        load(1, match)
-      },
-      "/cars-v2": (match) => {
-        renderTemplate(templateCarsBootstrap, "content")
-        loadV2(1, match)
+      "/example-nav": (match) => {
       }
     })
     .notFound(() => renderTemplate("No page for this route found", "content"))
